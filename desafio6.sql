3170cd91abf1f089e970d4ebbfd5ee0fe80e715f
Mauricio-OC <murisc0@hotmail.com>
2022-12-01 13:17:55 -0300

    FIX: REQ06

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 -- Active: 1669845737508@@127.0.0.1@3307@SpotifyClone
 SELECT
-MIN(sub.sub_price) AS faturamento_minimo,
-MAX(sub.sub_price) AS faturamento_maximo,
-ROUND(AVG(sub.sub_price), 2) AS faturamento_medio,
-ROUND(SUM(sub.sub_price), 2) AS faturamento_total
+FORMAT(MIN(sub.sub_price), 2) AS faturamento_minimo,
+FORMAT(MAX(sub.sub_price), 2) AS faturamento_maximo,
+FORMAT(AVG(sub.sub_price), 2) AS faturamento_medio,
+FORMAT(SUM(sub.sub_price), 2) AS faturamento_total
 FROM SpotifyClone.sub_id AS sub
 INNER JOIN SpotifyClone.user_id AS usuario
 ON sub.sub_id = usuario.sub_id;
