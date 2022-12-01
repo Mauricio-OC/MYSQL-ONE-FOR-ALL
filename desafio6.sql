9db779377d2d457142213baf4551913c54961262
Mauricio-OC <murisc0@hotmail.com>
2022-12-01 13:03:17 -0300

    feat: req06

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+-- Active: 1669845737508@@127.0.0.1@3307@SpotifyClone
+SELECT
+MIN(sub.sub_price) AS faturamento_minimo,
+MAX(sub.sub_price) AS faturamento_maximo,
+ROUND(AVG(sub.sub_price), 2) AS faturamento_medio,
+SUM(sub.sub_price) AS faturamento_total
+FROM SpotifyClone.sub_id AS sub
+INNER JOIN SpotifyClone.user_id AS usuario
+ON sub.sub_id = usuario.sub_id;
